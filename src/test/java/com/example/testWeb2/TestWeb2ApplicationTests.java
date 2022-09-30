c4b33ba43983eecd16a63f37acdfa9b9549c034a
unknown <hannaitbook@naver.com>
2022-09-30 12:43:45 +0900

    Initial commit

---
+++ src/test/java/com/example/testWeb2/TestWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testWeb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
